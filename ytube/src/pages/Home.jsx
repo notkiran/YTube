983fc2fbf48fee394b83b35282d7a2bf7cb2a0a9
Kiran G <mail@kirang.me>
2022-07-23 19:23:15 +0530

    nav and menu

---
+++ ytube/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components";
+import Card from "./../components/Card";
+
+const Container = styled.div``;
+
+const Home = () => {
+  return (
+    <Container>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </Container>
+  );
+};
+
+export default Home;
